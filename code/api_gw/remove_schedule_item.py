c030bb2541fad9641e732a7fec7ff2a1bfcf8ff7
Luka Krapic <luka.krapic@gmail.com>
2022-08-07 15:37:30 +0200

    Add dummy lambda functions for hadling rest calls
    New feature, add lambda handlers for api gateway.
    
    Partial implementation of lambda handlers for delete, create and
    retrieve schedule items.

---
+++ code/api_gw/remove_schedule_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from lib.requests_handler import RequestsHandler
+from lib.scheduler import DynamoScheduler
+
+# resources
+request_handler = RequestsHandler()
+scheduler = DynamoScheduler()
+
+
+def lambda_handler(event, contenxt):
+    # TODO: Parse event for schedule id
+    schedule_id = ""
+    request_handler.remove_schedule_item(schedule_id, scheduler)
