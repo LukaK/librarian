5a3438fa60707744346c15a4fbf51171d730f5bd
Luka Krapic <luka.krapic@gmail.com>
2022-08-09 14:50:30 +0200

    Add update schedule item api support
    New feature, api support for updating schedule item.

---
+++ code/api_gw/update_schedule_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from lib.requests_handler.data import LambdaProxyRequest
+from lib.requests_handler.requests_handler import RequestsHandler
+from lib.scheduler.scheduler import DynamoScheduler
+
+# resources
+request_handler = RequestsHandler()
+scheduler = DynamoScheduler()
+
+
+def lambda_handler(event, context):
+    lambda_proxy_event = LambdaProxyRequest(lambda_event=event)
+    return request_handler.update_schedule_item(lambda_proxy_event.payload, scheduler)
--- tests/test_api_gw_lambda.py
+++ tests/test_api_gw_lambda.py
@@ -73,3 +73,45 @@ def test__add_to_schedule_not_exists(dynamo_tables):
     }
     response = lambda_handler(lambda_event, None)
     assert response["status_code"] == 200
+
+
+def test__update_schedule_item_not_exists(dynamo_tables):
+    from api_gw.update_schedule_item import lambda_handler
+
+    lambda_event = {
+        "httpMethod": "POST",
+        "body": json.dumps(
+            {
+                "schedule_time": int(time.time()),
+                "workflow_arn": "test",
+                "schedule_id": "test",
+            }
+        ),
+    }
+    response = lambda_handler(lambda_event, None)
+    assert response["status_code"] == 400
+
+
+def test__update_schedule_item_exists(dynamo_tables):
+    from api_gw.update_schedule_item import lambda_handler
+    from lib.requests_handler.data import ScheduleRequest
+    from lib.scheduler.scheduler import DynamoScheduler
+
+    schedule_time = int(time.time())
+    schedule_request = ScheduleRequest(
+        workflow_arn="test_arn", schedule_time=schedule_time
+    )
+    schedule_item = DynamoScheduler.add_to_schedule(schedule_request)
+
+    lambda_event = {
+        "httpMethod": "POST",
+        "body": json.dumps(
+            {
+                "schedule_time": int(time.time()),
+                "workflow_arn": "test",
+                "schedule_id": schedule_item.schedule_id,
+            }
+        ),
+    }
+    response = lambda_handler(lambda_event, None)
+    assert response["status_code"] == 200
